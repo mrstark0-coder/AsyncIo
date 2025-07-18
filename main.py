47c66f2cb2f07c530b6ceb0bfcb0cf32c4b34734
Bhaskar Arya <bhashkararya2003@gmail.com>
2025-07-18 16:44:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import time
+import asyncio
+
+async def func1():
+    await asyncio.sleep(1)
+    print("func1")
+    return "L"
+
+async def func2():
+    await asyncio.sleep(1)
+    print("func2")
+    return "L"
+
+async def func3():
+    await asyncio.sleep(1)
+    print("func3")
+    return "L"
+
+# async def main():
+#     task = asyncio.create_task(func1())
+#     # await func1()
+#     await func2()
+#     await func3()
+
+async def main():
+    L = await asyncio.gather(
+        func1(),
+        func2(),
+        func3(),
+    )
+    print(L)
+
+
+asyncio.run(main())
